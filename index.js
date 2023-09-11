41af69bfbb5aae73a772a0f0c072b9802830c5d0
Vyachuk <Slavkoka045@gmail.com>
2023-09-11 12:59:26 +0300

    ADD: commander + remove comments

--- index.js
+++ index.js
@@ -1,12 +1,26 @@
+const { Command } = require("commander");
+const program = new Command();
+program
+  .option("-a, --action <type>", "choose action")
+  .option("-i, --id <type>", "user id")
+  .option("-n, --name <type>", "user name")
+  .option("-e, --email <type>", "user email")
+  .option("-p, --phone <type>", "user phone");
+
+program.parse(process.argv);
+
+const argv = program.opts();
+
 const contactsMethods = require("./contacts");
 
-async function invokeAction({ action, id, name, email, phone }) {
+// TODO: рефакторити
+const invokeAction = async ({ action, id, name, email, phone }) => {
   switch (action) {
     case "list":
       const allContacts = await contactsMethods.listContacts();
       return console.table(allContacts);
 
-    case "getById":
+    case "get":
       const contact = await contactsMethods.getContactById(id);
       return console.log(contact);
 
@@ -25,25 +39,6 @@ async function invokeAction({ action, id, name, email, phone }) {
     default:
       console.warn("\x1B[31m Unknown action type!");
   }
-}
+};
 
-// invokeAction({
-//   action: "add",
-//   name: "Ivan",
-//   email: "ada2Ldsasd",
-//   phone: "3434343434",
-// });
-
-// invokeAction({
-//   action: "list",
-// });
-
-// invokeAction({
-//   action: "getById",
-//   id: "05olLMgyVQdWRwgKfg5J6",
-// });
-
-// invokeAction({
-//   action: "remove",
-//   id: "AeHIrLTr6JkxGE6SN-0Rw",
-// });
+invokeAction(argv);
